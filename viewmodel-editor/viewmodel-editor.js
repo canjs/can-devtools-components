cfee171f4b0a7d58d76364d4ae7d462b50899d46
Kevin Phillips <kevin@bitovi.com>
2018-04-17 08:04:26 -0500

    viewmodel-editor component

--- viewmodel-editor/viewmodel-editor.js
+++ viewmodel-editor/viewmodel-editor.js
@@ -1,14 +1,35 @@
 import Component from "can-component";
+import canReflect from "can-reflect";
 import "viewmodel-editor/viewmodel-editor.less";
 
 export default Component.extend({
 	tag: "viewmodel-editor",
 	ViewModel: {
-		message: {
-			default: "This is the viewmodel-editor component"
+		tagName: "string",
+		viewModelData: "any",
+		setKeyValue: function(key, value) {
+			canReflect.setKeyValue(this.viewModelData, key, value);
 		}
 	},
 	view: `
-		<p>{{message}}</p>
+		{{#unless(tagName)}}
+            <h1>Select an Element to see its ViewModel</h1>
+        {{else}}
+            {{#unless(viewModelData)}}
+                <h1><{{tagName}}> does not have a ViewModel</h1>
+            {{else}}
+                <h1><{{tagName}}> ViewModel</h1>
+                <form>
+                    {{#each(viewModelData, key=key value=value)}}
+                        <p>
+                            {{key}}:
+                            <input
+                                value:from="value"
+                                on:change="scope.root.setKeyValue(key, scope.element.value)">
+                        </p>
+                    {{/each}}
+                </form>
+            {{/unless}}
+        {{/unless}}
 	`
 });
