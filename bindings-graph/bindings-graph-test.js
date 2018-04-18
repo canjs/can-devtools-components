80c70029032741900195045e00153e45c68c1358
Kevin Phillips <kevin@bitovi.com>
2018-04-18 09:42:01 -0500

    defaulting value of selectedKey when availableKeys is set

--- bindings-graph/bindings-graph-test.js
+++ bindings-graph/bindings-graph-test.js
@@ -6,8 +6,28 @@ const ViewModel = Component.ViewModel;
 const assert = chai.assert;
 
 describe("bindings-graph", () => {
-	it("VM", () => {
+	it("selectedKey defaults to first availableKey", () => {
 		const vm = new ViewModel();
-		assert.equal(typeof vm, "object");
+
+		// set up bindings
+		vm.on("selectedKey", () => {});
+
+		assert.ok(!vm.selectedKey, "selectedKey defaults to undefined");
+
+		vm.availableKeys.replace([ "one", "two", "three" ]);
+
+		assert.equal(vm.selectedKey, "one", "selectedKey defaults to availableKeys[0]");
+
+		vm.selectedKey = "four";
+
+		assert.equal(vm.selectedKey, "four", "selectedKey can be changed to a value not in availableKeys");
+
+		vm.availableKeys.replace([ "five", "six", "seven" ]);
+
+		assert.equal(vm.selectedKey, "five", "selectedKey defaults to availableKeys[0] even if it was already set");
+
+		vm.availableKeys.replace([]);
+
+		assert.notOk(vm.selectedKey, "selectedKey should be removed if there are no available keys");
 	});
 });
--- bindings-graph/bindings-graph.js
+++ bindings-graph/bindings-graph.js
@@ -8,9 +8,27 @@ export default Component.extend({
 	tag: "bindings-graph",
 	ViewModel: {
 		graphData: DefineMap,
-        availableKeys: DefineList,
+		availableKeys: { Type: DefineList, Default: DefineList },
 		selectedObj: "string",
-        selectedKey: "string",
+
+		selectedKey: {
+			type: "string",
+
+			value({ resolve, listenTo, lastSet }) {
+				// all key to be set
+				listenTo(lastSet, (setVal) => {
+					resolve(setVal);
+				});
+
+				listenTo(this.availableKeys, "length", (ev, newLength) => {
+					if (newLength) {
+						resolve( this.availableKeys[0] );
+					} else {
+						resolve(undefined);
+					}
+				});
+			}
+		},
 
 		connectedCallback(element) {
 			var drawGraph = function(data) {
