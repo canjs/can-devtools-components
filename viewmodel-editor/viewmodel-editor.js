de18f16467f699097871465393dc63b87684c9af
Kevin Phillips <kevin@bitovi.com>
2018-06-14 12:00:38 -0500

    adding setKeyValue function that should be called to update properties on source vm

--- viewmodel-editor/viewmodel-editor.js
+++ viewmodel-editor/viewmodel-editor.js
@@ -12,35 +12,38 @@ import 'spectre-canjs/sp-form/fields/sp-multi-check-field/sp-multi-check-field';
 export default Component.extend({
 	tag: "viewmodel-editor",
 	ViewModel: {
-        saving: 'boolean',
+		saving: 'boolean',
 		tagName: "string",
-        viewModelData: "any",
-        fields: {
-            get(){
-                const vm = this.viewModelData
-                return canReflect.getOwnEnumerableKeys(vm).map((key) => {
-                    const val = vm[key];
-                    return getBestFieldForValue(key, val)
-                });
-            }
-        },
-        updateViewModel(args){
-            canReflect.update(this.viewModelData, args[0]);
-            this.saving = false;
-        }
+		viewModelData: "any",
+		fields: {
+			get(){
+				const vm = this.viewModelData
+				return canReflect.getOwnEnumerableKeys(vm).map((key) => {
+					const val = vm[key];
+					return getBestFieldForValue(key, val)
+				});
+			}
+		},
+		updateViewModel(args){
+			canReflect.update(this.viewModelData, args[0]);
+			this.saving = false;
+		},
+		setKeyValue(key, value) {
+			canReflect.setKeyValue(this.viewModelData, key, value);
+		}
 	},
-    view: `
-        <div class="container">
+	view: `
+		<div class="container">
 		{{#unless(tagName)}}
-            <h1>Select an Element to see its ViewModel</h1>
-        {{else}}
-            {{#unless(viewModelData)}}
-                <h1><{{tagName}}> does not have a ViewModel</h1>
-            {{else}}
-                <h1><{{tagName}}> ViewModel</h1>
-                <sp-form object:from="viewModelData" fields:from="fields" isSaving:bind="saving" on:submit="updateViewModel(scope.arguments)"></sp-form>
-            {{/unless}}
-        {{/unless}}
-        </div>
+			<h1>Select an Element to see its ViewModel</h1>
+		{{else}}
+			{{#unless(viewModelData)}}
+				<h1><{{tagName}}> does not have a ViewModel</h1>
+			{{else}}
+				<h1><{{tagName}}> ViewModel</h1>
+				<sp-form object:from="viewModelData" fields:from="fields" isSaving:bind="saving" on:submit="updateViewModel(scope.arguments)"></sp-form>
+			{{/unless}}
+		{{/unless}}
+		</div>
 	`
 });
