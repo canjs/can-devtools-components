11dd97e6cb3abeac4908ea51c3fc757ccfdb27a7
Kevin Phillips <kevin@bitovi.com>
2018-06-19 13:00:27 -0500

    using jsoneditor for ViewModel Editor

--- viewmodel-editor/viewmodel-editor-test.js
+++ viewmodel-editor/viewmodel-editor-test.js
@@ -2,30 +2,149 @@ import "steal-mocha";
 import chai from "chai";
 import Component from "./viewmodel-editor";
 import DefineMap from "can-define/map/map";
+import canReflect from "can-reflect";
 
 let ViewModel = Component.ViewModel;
 let assert = chai.assert;
 
 describe("viewmodel-editor", () => {
-	it("can pass in a setKeyValue function for editing external VM", () => {
-		const realVM = new DefineMap({
+	let el, sourceVM, vm;
+
+	beforeEach((done) => {
+		sourceVM = new DefineMap({
 			foo: "abc",
 			bar: "xyz"
 		});
 
-		const vmEditor = new ViewModel({
-			viewModelData: realVM.serialize(),
-			setKeyValue: function(key, value) {
-				realVM[key] = value;
-			}
+		vm = new ViewModel({
+			viewModelData: sourceVM.serialize(),
+			tagName: "my-el"
 		});
 
-		assert.equal(vmEditor.viewModelData.foo, "abc", "vmEditor initial value");
-		assert.equal(realVM.foo, "abc", "realVM initial value");
+		el = document.createElement("viewmodel-editor");
+		document.body.appendChild(el);
 
-		vmEditor.setKeyValue("foo", "def");
+		setTimeout(() => {
+			vm.connectedCallback(el);
+			done();
+		}, 50);
+	});
 
-		assert.equal(vmEditor.viewModelData.foo, "abc", "vmEditor unchanged value");
-		assert.equal(realVM.foo, "def", "realVM changed value");
+	afterEach(() => {
+		document.body.removeChild(el);
+	});
+
+	describe("json", () => {
+		describe("when ONLY viewModelData is changing", () => {
+			it("should be set to initial viewModelData", () => {
+				assert.deepEqual(vm.json, sourceVM.serialize());
+			});
+
+			it("should be set to replaced viewModelData", () => {
+				vm.viewModelData = { aaa: "bbb", ccc: "ddd" };
+				assert.deepEqual(vm.json, { aaa: "bbb", ccc: "ddd" });
+			});
+
+			it("should be updated with updated viewModelData", () => {
+				vm.viewModelData = { aaa: "bbb", ccc: "ddd" };
+				vm.viewModelData.aaa = "zzz";
+				assert.deepEqual(vm.json, { aaa: "zzz", ccc: "ddd" });
+			});
+
+			it("should NOT be updated with updated viewModelData when updatesPaused === true", () => {
+				vm.viewModelData = { aaa: "bbb", ccc: "ddd" };
+				vm.updatesPaused = true;
+				vm.viewModelData.aaa = "yyy";
+				assert.deepEqual(vm.json, { aaa: "bbb", ccc: "ddd" });
+			});
+
+			it("should be updated with updated viewModelData when updatesPaused is set to false", () => {
+				vm.viewModelData = { aaa: "bbb", ccc: "ddd" };
+				vm.updatesPaused = true;
+				vm.viewModelData.aaa = "yyy";
+				vm.updatesPaused = false;
+				assert.deepEqual(vm.json, { aaa: "yyy", ccc: "ddd" });
+			});
+		});
+
+		describe("when viewModel AND jsoneditor data are changing", () => {
+			it("should merge changes made in jsoneditor with changes to viewModelData", () => {
+				// initial viewModelData
+				vm.viewModelData = { aaa: "yyy", ccc: "ddd" };
+
+				// simulate change to data in JSON editor
+				vm.json = { aaa: "yyy", ccc: "ddd" };
+				vm.editor.get = function() { return { aaa: "yyy", ccc: "eee" }; };
+
+				// pause updates
+				vm.updatesPaused = true;
+				// simulate change to viewModel data
+				vm.viewModelData.aaa = "xxx";
+				// unpause updates
+				vm.updatesPaused = false;
+
+				assert.deepEqual(vm.json, { aaa: "xxx", ccc: "eee" }, "should merge changes made in jsoneditor with changes to viewModelData");
+
+				vm.viewModelData.aaa = "www";
+				assert.deepEqual(vm.json, { aaa: "www", ccc: "eee" }, "should merge changes made to viewModelData into latest json in jsoneditor");
+			});
+		});
+	});
+
+	describe("getPatchedData", () => {
+		it("should work for add", () => {
+			let destination = { aaa: "bbb", ccc: "ddd" };
+			let oldSource = {};
+			let newSource = { eee: "fff" };
+			let expected = { aaa: "bbb", ccc: "ddd", eee: "fff" };
+			let actual = vm.getPatchedData(destination, oldSource, newSource);
+
+			assert.deepEqual(actual, expected);
+		});
+
+		it("should work for set", () => {
+			let destination = { aaa: "bbb", ccc: "ddd" };
+			let oldSource = { eee: "fff" };
+			let newSource = { eee: "ggg" };
+			let expected = { aaa: "bbb", ccc: "ddd", eee: "ggg" };
+			let actual = vm.getPatchedData(destination, oldSource, newSource);
+
+			assert.deepEqual(actual, expected);
+		});
+
+		it("should work for delete", () => {
+			let destination = { aaa: "bbb", ccc: "ddd" };
+			let oldSource = { aaa: "bbb", ccc: "ddd" };
+			let newSource = { aaa: "bbb" };
+			let expected = { aaa: "bbb" };
+			let actual = vm.getPatchedData(destination, oldSource, newSource);
+
+			assert.deepEqual(actual, expected);
+		});
+
+		it("should work for splice", () => {
+			let destination = { list: [ "one", "two" ] };
+			let oldSource = { list: [ "one", "two" ] };
+			let newSource = { list: [ "one", "three" ] };
+			let expected = { list: [ "one", "three" ] };
+			let actual = vm.getPatchedData(destination, oldSource, newSource);
+
+			assert.deepEqual(actual, expected);
+		});
+	});
+
+	it("saving", () => {
+		canReflect.onKeyValue(vm, "saving", () => {});
+
+		assert.equal(vm.saving, false, "false by default");
+
+		vm.updatesPaused = true;
+		assert.equal(vm.saving, false, "false while updates are paused");
+
+		vm.updatesPaused = false;
+		assert.equal(vm.saving, true, "true when updates are resumed");
+
+		vm.viewModelData = {};
+		assert.equal(vm.saving, false, "false once next update is received");
 	});
 });
--- viewmodel-editor/viewmodel-editor.js
+++ viewmodel-editor/viewmodel-editor.js
@@ -1,35 +1,146 @@
 import Component from "can-component";
-import canReflect from "can-reflect";
+import DefineMap from "can-define/map/map";
+import JSONEditor from "jsoneditor";
+import Observation from "can-observation";
+import diffDeep from "can-diff/deep/deep";
+
 import "viewmodel-editor/viewmodel-editor.less";
 
 export default Component.extend({
 	tag: "viewmodel-editor",
 	ViewModel: {
+		connectedCallback(element) {
+			let container = element.querySelector(".jsoneditor-container");
+			this.editor = new JSONEditor(container, {
+				mode: "code"/*,
+				modes: [ "code", "form", "tree" ],
+				search: false
+				history: false*/
+			});
+
+			this.listenTo("json", () => {
+				this.editor.set(this.json);
+			});
+
+			this.listenTo(document.querySelector(".ace_content"), "click", () => {
+				this.updatesPaused = true;
+			});
+
+			this.listenTo(window, "blur", () => {
+				this.updatesPaused = false;
+				this.updateValues(this.json);
+			});
+
+			return this.stopListening.bind(this);
+		},
+
 		tagName: "string",
-		viewModelData: "any",
-		setKeyValue: function(key, value) {
-			canReflect.setKeyValue(this.viewModelData, key, value);
+		viewModelData: { Type: DefineMap, Default: DefineMap },
+
+		updatesPaused: { type: "boolean", default: false },
+
+		saving: {
+			value({ listenTo, resolve }) {
+				resolve(false);
+
+				listenTo("updatesPaused", (ev, paused) => {
+					if (!paused) {
+						resolve(true);
+					}
+				});
+
+				listenTo("serializedViewModelData", () => {
+					resolve(false);
+				});
+			}
+		},
+
+		// allow overwriting editor for testing
+		editor: "any",
+
+		get serializedViewModelData() {
+			return this.viewModelData.serialize();
+		},
+
+		json: {
+			type: "any",
+
+			value({ listenTo, lastSet, resolve }) {
+				// allow manually overwriting json for testing
+				listenTo(lastSet, resolve);
+
+				// update json when viewModel is replaced or updated
+				// as long as updates are not paused
+				listenTo("serializedViewModelData", (ev, currentViewModelData, lastViewModelData) => {
+					if (!this.updatesPaused) {
+						let patchedData = this.getPatchedData(
+							this.json,
+							lastViewModelData,
+							currentViewModelData
+						);
+
+						resolve( patchedData );
+					}
+				});
+
+				// update with latest viewModel data when unpaused
+				listenTo("updatesPaused", (ev, paused) => {
+					if (!paused) {
+						let patchedData = this.getPatchedData(
+							this.serializedViewModelData,
+							this.json,
+							this.editor.get()
+						);
+
+						resolve( patchedData );
+					}
+				});
+
+				// set initial json to serialized viewModelData
+				resolve(this.serializedViewModelData);
+			}
+		},
+
+		getPatchedData(destination, oldSource, newSource) {
+			destination = Object.assign({}, destination);
+			let patches = diffDeep(oldSource, newSource);
+			patches.forEach(({ type, key, value, index, deleteCount, insert }) => {
+				switch(type) {
+					case "add":
+					case "set":
+						destination[key] = value;
+						break;
+					case "delete":
+						delete destination[key];
+						break;
+					case "splice":
+						destination[key].splice(index, deleteCount, ...insert);
+						break;
+				}
+			});
+
+			return destination;
+		},
+
+		updateValues(data) {
+			console.log("updating viewModel with", data);
 		}
 	},
 	view: `
-		{{#unless(tagName)}}
+		{{# unless(tagName) }}
             <h1>Select an Element to see its ViewModel</h1>
-        {{else}}
-            {{#unless(viewModelData)}}
+        {{ else }}
+            {{# unless(viewModelData) }}
                 <h1><{{tagName}}> does not have a ViewModel</h1>
-            {{else}}
+            {{ else }}
                 <h1><{{tagName}}> ViewModel</h1>
-                <form>
-                    {{#each(viewModelData, key=key value=value)}}
-                        <p>
-                            {{key}}:
-                            <input
-                                value:from="value"
-                                on:change="scope.vm.setKeyValue(key, scope.element.value)">
-                        </p>
-                    {{/each}}
-                </form>
-            {{/unless}}
-        {{/unless}}
+            {{/ unless }}
+        {{/ unless }}
+
+		<div class="jsoneditor-container {{#if(saving)}}disabled{{/if}}"></div>
+
+		{{# if(tagName) }}
+			<button>Save</button>
+		{{/if}}
 	`
 });
