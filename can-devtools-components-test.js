0fca7c607f05c670e625fc81aff9103e78089a9a
Kevin Phillips <kevin@bitovi.com>
2019-01-08 16:42:55 -0600

    fixing test setup

--- can-devtools-components-test.js
+++ can-devtools-components-test.js
@@ -1,4 +1,7 @@
-import "viewmodel-editor/viewmodel-editor-test";
-import "queues-logstack/queues-logstack-test";
 import "bindings-graph/bindings-graph-test";
+import "component-tree/component-tree-test";
+import "editable-span/editable-span-test";
 import "json-tree-editor/json-tree-editor-test";
+import "panel/panel-test";
+import "queues-logstack/queues-logstack-test";
+import "viewmodel-editor/viewmodel-editor-test";
