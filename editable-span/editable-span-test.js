5518a43b1a267509283fe1a1847609b70f8677d0
Kevin Phillips <kevin@bitovi.com>
2019-02-01 15:29:00 -0600

    fixing extra spaces inside editable-span

--- editable-span/editable-span-test.js
+++ editable-span/editable-span-test.js
@@ -1,40 +1,57 @@
 import "steal-mocha";
 import chai from "chai";
-import Component from "./editable-span";
+import EditableSpan from "./editable-span";
 
-const ViewModel = Component.ViewModel;
+const ViewModel = EditableSpan.ViewModel;
 const assert = chai.assert;
 
 const noop = () => { };
 const ev = { stopPropagation: noop };
 
 describe("editable-span", () => {
-	it("edit() / save()", () => {
-		const vm = new ViewModel();
-		vm.listenTo("editing", () => {});
-		vm.listenTo("text", () => {});
+	describe("ViewModel", () => {
+		it("edit() / save()", () => {
+			const vm = new ViewModel();
+			vm.listenTo("editing", () => {});
+			vm.listenTo("text", () => {});
 
-		vm.edit(ev);
+			vm.edit(ev);
 
-		assert.ok(vm.editing, "edit() sets editing to true");
+			assert.ok(vm.editing, "edit() sets editing to true");
 
-		vm.save("foo");
-		assert.notOk(vm.editing, "save() sets editing to false");
-		assert.equal(vm.text, "foo", "save() sets text");
+			vm.save("foo");
+			assert.notOk(vm.editing, "save() sets editing to false");
+			assert.equal(vm.text, "foo", "save() sets text");
+		});
+
+		it("showOptions", () => {
+			const vm = new ViewModel();
+
+			assert.notOk(vm.showOptions, "showOptions defaults to false");
+
+			vm.editing = true;
+			assert.notOk(vm.showOptions, "showOptions === false when editing === true without options");
+
+			vm.options = [ "one" ];
+			assert.ok(vm.showOptions, "showOptions === true when editing === true with options");
+
+			vm.editing = false;
+			assert.notOk(vm.showOptions, "showOptions === false when editing === false with options");
+		});
 	});
 
-	it("showOptions", () => {
-		const vm = new ViewModel();
+	describe("Component", () => {
+		it("renders correctly", () => {
+			const c = new EditableSpan({
+				viewModel: {
+					text: "some text"
+				}
+			});
 
-		assert.notOk(vm.showOptions, "showOptions defaults to false");
+			const el = c.element;
+			const valueSpan = el.querySelector("span");
 
-		vm.editing = true;
-		assert.notOk(vm.showOptions, "showOptions === false when editing === true without options");
-
-		vm.options = [ "one" ];
-		assert.ok(vm.showOptions, "showOptions === true when editing === true with options");
-
-		vm.editing = false;
-		assert.notOk(vm.showOptions, "showOptions === false when editing === false with options");
+			assert.equal(valueSpan.innerText, "some text", "renders text without spaces");
+		});
 	});
 });
